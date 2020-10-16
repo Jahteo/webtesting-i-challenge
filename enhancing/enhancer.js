08f100d89a66225da51312d61fefe95ff5058800
Josh Glantz-Hucks <jahteo@gmail.com>
2020-10-15 22:18:43 -0700

    MVP!

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -13,19 +13,29 @@ module.exports = {
 
 //returns a new tien obj w enhancement increased by 1, max 20, no change to durability
 function success(item) {
-  return { ...item };
+  return (
+    item.enhancement<20 ? { ...item, enhancement: item.enhancement+1 }
+    : {...item}
+  )
 }
 
 //returns a new item obj. if enhancement < 15, durability is decreased by 5.
 //if enhancement >= 15, durability is decreased by 10
 //if enhancement lvl is > 16, lvl goes down by 1
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15) {
+      return {...item, durability: item.durability-5}
+    } else if (item.enhancement === 15) {
+      return {...item, durability: item.durability-10}
+    } else {
+      return {...item, durability: item.durability-10, enhancement: item.enhancement-1}
+    }
+
 }
 
 //accepts an item object, returns a new item object w durability restored to 100
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,50 @@
-const enhancer = require('./enhancer.js');
+const { success, fail, repair, get } = require('./enhancer.js');
 // test away!
+
+
+item = {
+  name: "",
+  durability: "", //0-100
+  enhancement: "" //0-20
+}
+sadItem = {
+  name: "sward of trooth",
+  durability: 5, //0-100
+  enhancement: 0 //0-20
+}
+epicItem = {
+  name: "SWORD OF AWESOME",
+  durability: 97, //0-100
+  enhancement: 20 //0-20
+}
+
+describe("repair()", () => {
+  it("should return an object w duribility = 100", () => {
+    expect(repair(sadItem).durability).toEqual(100)
+  })
+})
+
+describe("success()", () => {
+  it("should increase enhancement by 1", () => {
+    expect(success(sadItem).enhancement).toBe(1)
+  })
+  it("should not increase enhancement above 20", () => {
+    expect(success(epicItem).enhancement).toBe(20)
+  })
+  it("should not change durability", () => {
+    expect(success(sadItem).durability).toBe(5)
+    expect(success(epicItem).durability).toBe(97)
+  })
+})
+
+describe("fail()", () => {
+  it("should decrease durability by 5 if enhancement is less that 15", () => {
+    expect(fail(sadItem).durability).toBe(0)
+  })
+  it("should decrease durability by 10 if enhancement is 15 or more", () => {
+    expect(fail(epicItem).durability).toBe(87);
+  })
+  it("should decrease enhancement by 1 if item.enchancement is greater than 16", () => {
+    expect(fail(epicItem).enhancement).toBe(19)
+  })
+})
